ac638a6b84e309f4346eef8e485d3244287da9ff
Negin Mahrang <neginmahrang@Negins-MacBook-Air.local>
2024-04-20 20:53:32 -0700

    Added sudoku and hard array quesrions.

---
+++ Arrays/Longest-Consecutive-Sequence.py
@@ -0,0 +1,35 @@
+"""
+Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+You must write an algorithm that runs in O(n) time.
+Example 1:
+Input: nums = [100,4,200,1,3,2]
+Output: 4
+Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+Example 2:
+Input: nums = [0,3,7,2,5,8,4,6,0,1]
+Output: 9
+"""
+
+
+from typing import List
+
+# Solve this question without SROTING: since sorting has nlogn complexity 
+# therefore it's not the most optimal solution
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        #turn the list into set to get rid of dups
+        numsSet=set(nums)
+        longest=0
+        count=0
+        for n in numsSet:
+            count+=1
+            if n-1 not in numsSet: # *** it will only process the starting numbers meaning 0 and 15
+                length=0
+                while n+length in numsSet:
+                    length+=1
+                    count+=1
+                longest=max(length,longest)
+        return longest
+
+s=Solution()
+print(s.longestConsecutive([9,3,7,2,5,8,0,15,16,17,18,19,20,3,4,6,0,1])) #complexity O(n) jus because of line that I put *** in.
---
+++ Arrays/valid-sudoku.py
@@ -0,0 +1,47 @@
+"""
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+1. Each row must contain the digits 1-9 without repetition. (use HashSet)
+2. Each column must contain the digits 1-9 without repetition. (use HashSet)
+3. Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+Note:
+
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+
+"""
+
+import collections
+from typing import List
+
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        cols= collections.defaultdict(set)
+        rows=collections.defaultdict(set)
+        squares=collections.defaultdict(set)
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                if (board[r][c] in rows[r] or
+                   board[r][c] in cols[c] or
+                    board[r][c] in squares[(r//3,c//3)] ): #see Neetcode youtube
+                    return False
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r//3,c//3)].add(board[r][c])
+                #The reason add is used instead of = when adding elements to the sets is because add is specifically designed to add an element to a set without replacing any existing elements. If the element already exists in the set, add does nothing and doesn't raise an error.
+        return True
+sol=Solution()
+board = [["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+print(sol.isValidSudoku(board))
